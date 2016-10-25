d42fd0d62579c2a40853b132a7b9683143b228c5
Sergio Sebastian Arriagada <sergio@hashdog.com>
2016-10-25 13:50:09 -0300

    modify template js

--- js/template.js
+++ js/template.js
@@ -1,54 +1,14 @@
-var WHITE_ICON = './images/icon-white.svg';
-var GRAY_ICON = './images/icon-gray.svg';
-
-var parkMap = {
-  acad: 'Acadia National Park',
-  arch: 'Arches National Park',
-  badl: 'Badlands National Park',
-  brca: 'Bryce Canyon National Park',
-  crla: 'Crater Lake National Park',
-  dena: 'Denali National Park',
-  glac: 'Glacier National Park',
-  grca: 'Grand Canyon National Park',
-  grte: 'Grand Teton National Park',
-  olym: 'Olympic National Park',
-  yell: 'Yellowstone National Park',
-  yose: 'Yosemite National Park',
-  zion: 'Zion National Park'
-};
-
-var cardButtonCallback = function(t){
-  var items = Object.keys(parkMap).map(function(parkCode){
-    var urlForCode = 'http://www.nps.gov/' + parkCode + '/';
-    return {
-      text: parkMap[parkCode],
-      url: urlForCode,
-      callback: function(t){
-        return t.attach({ url: urlForCode, name: parkMap[parkCode] })
-        .then(function(){
-          return t.closePopup();
-        })
-      }
-    };
-  });
-
-  return t.popup({
-    title: 'Popup Search Example',
-    items: items,
-    search: {
-      count: 5,
-      placeholder: 'Search National Parks',
-      empty: 'No parks found'
-    }
-  });
-};
-
 TrelloPowerUp.initialize({
-  'card-buttons': function(t, options) {
+  'card-buttons': function(t, options){
     return [{
-      icon: GRAY_ICON,
-      text: 'Template',
-      callback: cardButtonCallback
+      icon: './images/icon-white.svg',
+      text: 'Button Test',
+      callback: function(t){
+        return t.popup({
+          title: "Card Button Popup",
+          url: './overlay.html'
+        });
+      }
     }];
   },
   'show-settings': function(t, options){
