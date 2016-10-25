6dcd298f781b04f9af330ebb6c87abc230d65a58
Sergio Sebastian Arriagada <sergio@hashdog.com>
2016-10-25 12:57:52 -0300

    update power up

--- js/template.js
+++ js/template.js
@@ -10,12 +10,5 @@ TrelloPowerUp.initialize({
         });
       }
     }];
-  },
-  'show-settings': function(t, options){
-    return t.popup({
-      title: 'Settings',
-      url: './settings.html',
-      height: 184
-    });
   }
 });
