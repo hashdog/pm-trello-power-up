1d99c14167b784fe45c17e0360b0585bf34b9950
Sergio Sebastian Arriagada <sergio@hashdog.com>
2016-10-27 11:50:33 -0300

    fix card data

--- js/template.js
+++ js/template.js
@@ -1,34 +1,36 @@
-var userName = document.getElementsByClassName("js-member-name")[0].textContent;
-console.log('ok');
-console.log('userName: ', userName);
+(){
+  var userName = document.getElementsByClassName("js-member-name")[0].textContent;
+  console.log('ok');
+  console.log('userName: ', userName);
 
-TrelloPowerUp.initialize({
-  'card-buttons': function(t, options){
-    return [{
-      icon: './images/icon-gray.svg',
-      text: 'Time track',
-      callback: function(t){
-        return t.overlay({
-          title: 'Time track',
-          url: './overlay.html',
-          args: { user: userName }
-        });
-      }
-    }];
-  },
-  'card-badges': function(t, card) {
-    return [
-      {
+  TrelloPowerUp.initialize({
+    'card-buttons': function(t, options){
+      return [{
         icon: './images/icon-gray.svg',
-        text: '0'
-      }
-    ];
-  },
-  'show-settings': function(t, options){
-    return t.popup({
-      title: 'Google Form Settings',
-      url: './settings.html',
-      height: 184
-    });
-  }
-});
+        text: 'Time track',
+        callback: function(t){
+          return t.overlay({
+            title: 'Time track',
+            url: './overlay.html',
+            args: { user: userName }
+          });
+        }
+      }];
+    },
+    'card-badges': function(t, card) {
+      return [
+        {
+          icon: './images/icon-gray.svg',
+          text: '0'
+        }
+      ];
+    },
+    'show-settings': function(t, options){
+      return t.popup({
+        title: 'Google Form Settings',
+        url: './settings.html',
+        height: 184
+      });
+    }
+  });
+}
