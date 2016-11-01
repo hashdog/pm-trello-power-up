121854e99cd17c24356b1d5bf77da9c50c715abd
Lizardo Celiz Rafael Alejandro <lizardoceliz@gmail.com>
2016-11-01 18:41:58 -0300

    SpreadSheet
      - Query test.

--- js/estimate.js
+++ js/estimate.js
@@ -1,5 +1,6 @@
 console.log('estimate.js');
-var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1raLEpgZyX0LHszBYtkR25N4t4EGbiYF3Os1fpRu5DDA/edit#gid=38447568'
+// var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1raLEpgZyX0LHszBYtkR25N4t4EGbiYF3Os1fpRu5DDA/edit#gid=38447568'
+var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/17EwNIgGf9YqVUgl0WQftCJybGy2cYlKk7PKeJi0T3qU/edit#gid=0'
 
 var myCallback = function (error, options, response) {
   if (error) {
@@ -12,7 +13,7 @@ var myCallback = function (error, options, response) {
 
 $('#switch-hitters').sheetrock({
   url: mySpreadsheet,
-  query: "select B  where B = 'hernan' order by L desc",
-  fetchSize: 2,
+  query: "select A,B,C,D,E,L where E = 'Both' order by L desc",
+  fetchSize: 10
   callback: myCallback
 });
