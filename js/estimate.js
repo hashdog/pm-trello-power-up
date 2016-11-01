812d847c29214a2d0ce6066d2620fd9e0f5bbae3
Lizardo Celiz Rafael Alejandro <lizardoceliz@gmail.com>
2016-11-01 18:12:03 -0300

    SpreadSheet
      - Log spread response errors.

--- js/estimate.js
+++ js/estimate.js
@@ -1,6 +1,15 @@
 console.log('estimate.js');
 var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/1raLEpgZyX0LHszBYtkR25N4t4EGbiYF3Os1fpRu5DDA/edit#gid=38447568'
 
+var myCallback = function (error, options, response) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(response.data);
+    console.log(response.html);
+  }
+};
+
 $('#switch-hitters').sheetrock({
   url: mySpreadsheet,
   query: "select A,B,C",
