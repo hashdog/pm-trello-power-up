52bf392a487e958b033ffa1b483ac7bb8bfab82d
Lizardo Celiz Rafael Alejandro <lizardoceliz@gmail.com>
2016-11-01 17:40:35 -0300

    Spreadsheet
      - Add SheetRock libray in order to do google spreadsheet queries.
      - Add query example.

--- js/estimate.js
+++ js/estimate.js
@@ -1,1 +1,8 @@
 console.log('estimate.js');
+var mySpreadsheet = 'https://drive.google.com/accounts?continueUrl=https://docs.google.com/a/hashdog.com/spreadsheets/d/1raLEpgZyX0LHszBYtkR25N4t4EGbiYF3Os1fpRu5DDA/edit?usp%3Dforms_web_b%23gid%3D38447568&ddrp=1#'
+
+$('#switch-hitters').sheetrock({
+  url: mySpreadsheet,
+  query: "select A,B,C,D",
+  fetchSize: 10
+});
