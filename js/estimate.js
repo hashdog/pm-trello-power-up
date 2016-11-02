6c9bc627a986a202e8fbfbc1ca95c131a4d9ee7e
Sergio Sebastian Arriagada <sergio@hashdog.com>
2016-11-02 11:52:39 -0300

    fix estimate.js

--- js/estimate.js
+++ js/estimate.js
@@ -4,8 +4,6 @@ var t = TrelloPowerUp.iframe();
 // this function we be called once on initial load
 // and then called each time something changes
 t.render(function(){
-  console.log('estimate.js');
-
   var mySpreadsheet = 'https://docs.google.com/spreadsheets/d/17EwNIgGf9YqVUgl0WQftCJybGy2cYlKk7PKeJi0T3qU/edit#gid=0'
 
   $('#switch-hitters').sheetrock({
@@ -13,6 +11,7 @@ t.render(function(){
     query: "select A,B,C,D,E,L where E = 'Both' order by L desc",
     fetchSize: 10
   });
+
 });
 
 // close overlay if user clicks outside our content
